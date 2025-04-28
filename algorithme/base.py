7520cd5593e573e562447d97cb16de1a1995562e
papaousseynouba@esp.sn <papaousseynouba@esp.sn>
2025-04-28 23:18:16 +0000

    les diffeerentes opérations arithméthique qu'il y a et les differentes opérations de compraraison

---
+++ algorithme/base.py
@@ -0,0 +1,16 @@
+x = 1
+y = 2.5
+
+#Arithmétique
+print(x + y)
+print(x * y)
+print(x - y)
+print(x / y)
+print(x ** y)
+
+#Comparaison
+print(x == y)
+print(x >= y)
+print(x <= y)
+print(x > y)
+print(x < y)
